ad6e2d333f422c57b3178c851c457c0e3fa4a269
Романов Денис <mr.deaddude@mail.ru>
2024-09-18 18:02:28 +0700

    допилен парсер jsonов и последующее логирование инфы в csv/excel таблицу

--- downloader_project/data_extractor.py
+++ downloader_project/data_extractor.py
@@ -4,9 +4,13 @@ from pandas.core.interchange.dataframe_protocol import DataFrame
 
 
 class CustomDate:
-    def __init__(self,dates_list = None, hours_list = None):
-        self.dates_list = dates_list if dates_list is not None else []
-        self.hours_list = hours_list if hours_list is not None else []
+    def __init__(self,df = None,dates_list = None, hours_list = None):
+        if df is not None:
+            self.dates_list = df['date'].apply(lambda x: x.split()[0]).tolist()
+            self.hours_list = df['date'].apply(lambda x: x.split()[1][:8]).tolist()
+        else:
+            self.dates_list = dates_list if dates_list is not None else []
+            self.hours_list = hours_list if hours_list is not None else []
 
     def add_to_dates(self, item):
         self.dates_list.append(item)
@@ -32,21 +36,9 @@ class CustomDate:
         else:
             raise IndexError("Индекс находится вне диапазона списка hours_list")
 
-
-def get_customdate_object(csv_file: DataFrame) -> CustomDate:
-    df = pd.read_csv(csv_file)
-    date_list = df['date'].to_list()
-    custom_date_obj = CustomDate()
-    for i in range(len(date_list)):
-        custom_date_obj.add_to_dates(date_list[i].split()[0])
-        custom_date_obj.add_to_hours(date_list[i].split()[1][:8])
-    return custom_date_obj
-
-custom_date_object = get_customdate_object('target.csv')
-#print(custom_get_object.get_dates_list())
-#print(custom_get_object.get_hours_list())
-
-
-df = pd.read_csv('target.csv')
-date_list = df['date'].to_list()
-#print(date_list)
+    def get_custom_object_from_csv(self, csv_file_name: str):
+        df = pd.read_csv(csv_file_name)
+        date_list = df['date'].to_list()
+        for date in date_list:
+            self.add_to_dates(date.split()[0])
+            self.add_to_hours(date.split()[1][:8])
--- downloader_project/html_parser.py
+++ downloader_project/html_parser.py
@@ -1,38 +1,71 @@
 import requests
-import re
-from data_extractor import custom_date_object
-from bs4 import BeautifulSoup
+import pandas as pd
+from tqdm import tqdm
+from data_extractor import  CustomDate
+from pandas.core.interchange.dataframe_protocol import DataFrame
+from datetime import datetime, timedelta, timezone
 
+
+
+def parse_json(product: str, date: str) -> dict | int:
+    standard_parse_url = f'https://ladsweb.modaps.eosdis.nasa.gov/api/v2/content/details?products={product}&temporalRanges={date}&regions=[BBOX]W84.8%20N56.7%C2%A0E85.1%C2%A0S56.4'
+    response = requests.get(url=standard_parse_url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return response.status_code
+
+def get_closest_time_row(product: str,date: str, time: str) -> dict:
+    response_ = parse_json(product,date)
+    if isinstance(response_, int):
+        return {'status_code_myd': response_}
+    elif isinstance(response_, dict):
+        min_time_range = timedelta.max
+        closest_item = None
+        closest_time_range = None
+        for item in response_['content']:
+            lidar_time = date + ' ' + time
+            lidar_time = datetime.strptime(lidar_time, "%Y-%m-%d %H:%M:%S")
+            target_time = datetime.strptime(item['start'], "%Y-%m-%d %H:%M:%S").replace(tzinfo=timezone.utc)
+            lidar_time = lidar_time.replace(tzinfo=timezone(timedelta(hours=7)))
+            time_range = abs(lidar_time - target_time)
+            if time_range < min_time_range:
+                min_time_range = time_range
+                closest_item = item
+                closest_time_range = time_range
+        return {'lidar_date':lidar_time.strftime("%Y-%m-%d %H:%M:%S"),'name': closest_item['name'],'product':closest_item['products'],
+                'illuminations':closest_item['illuminations'],'start': closest_item['start'],'md5sum':closest_item['md5sum'],
+                'download_link':closest_item['downloadsLink'],'time_range':str(closest_time_range)}
+
+def fill_table(df: DataFrame, custom_date_object: CustomDate, products: list[str]) -> DataFrame:
+    i = 0
+    with tqdm(total= len(custom_date_object.get_dates_list())) as pbar:
+        for date,time in tqdm(zip(custom_date_object.get_dates_list(),custom_date_object.get_hours_list())):
+            row_myd = get_closest_time_row(products[0], date, time)
+            row_mod = get_closest_time_row(products[1], date, time)
+            df = pd.concat([df, pd.DataFrame([row_myd])], ignore_index=True)
+            df = pd.concat([df, pd.DataFrame([row_mod])], ignore_index=True)
+            pbar.update(1)
+        return df
+
+
+
+
+
+
+
+#variables
+csv_file_name = 'target.csv'
+dates_df = pd.read_csv(csv_file_name)
+custom_date = CustomDate(dates_df)
 URL = 'https://ladsweb.modaps.eosdis.nasa.gov/search/order/4/MYD021KM--61,MOD021KM--61/date/DB/84.8,56.7,85.1,56.4'
 TEST_URL = 'https://ladsweb.modaps.eosdis.nasa.gov/search/order/4/MYD021KM--61,MOD021KM--61/2021-10-17..2021-10-17/DB/84.8,56.7,85.1,56.4'
-JSON_URL = 'https://ladsweb.modaps.eosdis.nasa.gov/api/v2/content/details?products=MOD021KM&temporalRanges=2021-10-17..2021-10-17&regions=[BBOX]W84.8 N56.7 E85.1 S56.4 '
-
-response = requests.get(JSON_URL)
-data = response.json()
-print(data)
-
-from urllib import request
+products = ['MYD021KM','MOD021KM']
+columns = ['lidar_date','name','product','illuminations','start','md5sum','download_link','time_range']
+files_table = pd.DataFrame(columns=columns)
 
 
-local_filename = 'downloaded_file'
-def download_progress_hook(count, block_size, total_size):
-    """
-    Функция обратного вызова, используемая для отслеживания прогресса скачивания.
-    :param count: Количество загруженных блоков
-    :param block_size: Размер блока в байтах
-    :param total_size: Общий размер файла
-    """
-    total_downloaded = count * block_size
-    percent = (total_downloaded / total_size) * 100
-    print(f"\rСкачано {total_downloaded} из {total_size} байт ({percent:.2f}%)", end='')
-def download_file(url, filename):
-    # Загружаем файл и сохраняем его по указанному пути, передавая функцию обратного вызова
-    request.urlretrieve(url, filename, reporthook=download_progress_hook)
-
-#@download_file('https://ladsweb.modaps.eosdis.nasa.gov/api/v2/content/archives/MOD021KM.A2021290.1555.061.2021291013411.hdf', local_filename)
-# response = requests.get(TEST_URL)
-# if response.status_code == 200:
-#     html_content = response.text
-#     print(html_content)
-# else:
-#     print(f'Ошибка: {response.status_code}')
+files_table = fill_table(files_table,custom_date,products)
+files_table.to_csv('nasa_files.csv',index = False)
+#print(get_closest_time_row('MYD021KM',custom_date.get_dates_list()[455],custom_date.get_hours_list()[455]))
+# print(json_['content'])
